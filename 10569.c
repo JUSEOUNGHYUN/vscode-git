d7c1094535e3e4b4b5fa63a2349194fce06cf28c
JUSEOUNGHYUN <succeed3256@naver.com>
2021-05-08 02:50:12 +0900

    다면체.file

---
+++ 10569.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    int a,b,c;
+    for(int i=0; i<n; i++) {
+        scanf("%d %d",&a,&b);
+
+    c=2-a+b;
+    printf("%d\n",c);
+    }
+}
