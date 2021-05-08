3daba2bf854681db6a461d24b59d872db60784a2
JUSEOUNGHYUN <succeed3256@naver.com>
2021-05-08 22:24:21 +0900

    나는요리사다.file

--- 2953.c
+++ 2953.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
 int main() {
+	
+	int a,b,c,d;
+	int sum;
+	int max=0,count;
+	for(int i=0; i<5; i++) {
+		sum=0;
+		scanf("%d %d %d %d",&a,&b,&c,&d);
+		sum=a+b+c+d;
 
-    int a;
-    scanf("%d",&a);
-    int b,c;
-    double sum=0;
-    for(int i=0; i<a; i++) {
-
-        scanf("%d %d",&b,&c);
-        sum+=b%c;
-    }
-    printf("%.2f", &sum);
+		if(sum>max){
+		max=sum;
+		count=i;
+	}
+	}
+	printf("%d %d",count+1,max);
+	
+	
 }
