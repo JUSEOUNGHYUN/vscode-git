575ab8c8fa86081b6d518380d9642ebf28fb8b7a
JUSEOUNGHYUN <succeed3256@naver.com>
2021-05-21 20:41:14 +0900

    로봇.file

---
+++ 5339.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+
+
+	printf("     /~\\\n");
+
+	printf("    ( oo|\n");
+
+	printf("    _\\=/_\n");
+
+	printf("   /  _  \\\n");
+
+	printf("  //|/.\\|\\\\ \n");
+
+	printf(" ||  \\ /  ||\n");
+
+	printf("============\n");
+
+	printf("|          |\n");
+
+	printf("|          |\n");
+
+	printf("|          |\n");
+
+
+
+}
