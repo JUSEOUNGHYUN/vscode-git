e067bc6d37dc6ac80584bbf0e88bfdf405d09239
succeed3256@naver.com <succeed3256@naver.com>
2021-06-24 19:35:38 +0900

    더하기.file

---
+++ 1001.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+
+    int a,b;                        // a,b라는 변수를 선언하고
+    scanf("%d %d", &a, &b); // scanf로 입력을 받는다.
+
+    printf("%d\n", a - b);      // printf로 출력한다.
+}
