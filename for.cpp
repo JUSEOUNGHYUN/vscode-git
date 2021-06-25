2502d96c792434fa0a64f674fbd23d7e0ccf3cb0
succeed3256@naver.com <succeed3256@naver.com>
2021-06-25 15:40:07 +0900

    x보다작은 a만 출력.file

---
+++ for.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n,a,x;
+    cin >> n >> x;
+    int count;
+
+    for(int i=0; i<n; i++){
+        cin>>a;
+        
+        if(x>a)
+        cout<<a<<" ";
+        
+
+    }
+
+
+}
