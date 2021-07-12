be81501c4b7befb832d5001f416227dfa8ee3862
succeed3256@naver.com <succeed3256@naver.com>
2021-07-12 11:31:39 +0900

    파티장 넓이 참가자수 수용.file

---
+++ 2845.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n,l;
+    cin>>n>>l;
+
+    int p;
+
+    for(int i=0; i<5; i++){
+        cin>>p;
+
+            cout<<p-n*l<<" ";
+    }
+
+    
+}
