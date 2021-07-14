7c80e4077fca5606dd933bdcefa9c276b7515abc
succeed3256@naver.com <succeed3256@naver.com>
2021-07-14 18:00:23 +0900

    사계절.file

---
+++ 1070cu.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a;
+    cin>>a;
+
+    if(a==12||a==1||a==2){
+        cout<<"winter";
+    }
+    else if(a==3||a==4||a==5){
+        cout<<"spring";
+    }
+    else if(a==6||a==7||a==8){
+        cout<<"summer";
+    }
+    else if(a==9||a==10||a==11){
+        cout<<"fall";
+    }
+}
