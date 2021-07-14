e93a4b0c45ca28246437a340efd0220d659e61b8
succeed3256@naver.com <succeed3256@naver.com>
2021-07-14 17:55:18 +0900

    스위치문 문자입력받아서 문자열 출력.file

---
+++ 1065.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char c;
+    cin>>c;
+
+    switch (c)
+    {
+    case 'A':
+    cout<<"best!!!";
+    break;
+    case 'B':
+    cout<<"good!!";
+    break;
+    case 'C':
+    cout<<"run!";
+    break;
+    case 'D':
+    cout<<"slowly~";
+    break;
+    default:
+    cout<<"what?";
+    break;
+    }
+    return 0;
+}
