cf7be475772c413be04f0a86ce2b7edcbdebc835
succeed3256@naver.com <succeed3256@naver.com>
2021-07-15 10:45:15 +0900

    문자n을 받아 a부터 n까지 알파벳순으로 나열.file

---
+++ 1076cu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char c;
+    cin>>c;
+    char a='a';
+
+    do{
+        cout<<a<<" ";
+        a++;
+        c--;
+    }while(c>='a');
+}
