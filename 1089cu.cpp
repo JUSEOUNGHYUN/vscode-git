da70c563490e65faadbac8eb5b7f82e739c0dc1d
succeed3256@naver.com <succeed3256@naver.com>
2021-07-15 14:14:35 +0900

    등차수열.file

---
+++ 1089cu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    for(int i=1; i<c; i++){
+        a+=b;
+    }
+
+    cout<<a;
+}
