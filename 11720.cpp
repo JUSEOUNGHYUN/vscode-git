e76be9f19f99287ba90ee4e842b93fb274463d2e
succeed3256@naver.com <succeed3256@naver.com>
2021-07-22 21:29:38 +0900

    숫자를 문자열처럼 입력하고 총합을 구하는 코드.file

---
+++ 11720.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    int n;
+    int sum=0;
+    char a[100]={0,};
+
+    cin>>n;
+    cin>>a;
+
+    for(int i=0; i<n; i++){
+        sum+=a[i]-'0';
+    }
+
+    cout<<sum;
+}
