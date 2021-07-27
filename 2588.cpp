c54c554533228e473f4e866fb71810164510f60d
succeed3256@naver.com <succeed3256@naver.com>
2021-07-27 18:52:43 +0900

    a와 b1의자리,10,100의자리 a*b 구하는코드.file

---
+++ 2588.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a,b;
+    cin>>a>>b;
+
+    cout<<a*(b%10%10)<<"\n";
+    cout<<a*(b/10%10)<<"\n";
+    cout<<a*(b/10/10)<<"\n";
+    cout<<a*b;
+    
+}
