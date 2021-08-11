0d2777e58930ed8ec4b8a48f236159152d1a796a
succeed3256@naver.com <succeed3256@naver.com>
2021-08-11 14:26:02 +0900

    codeup 1280.file

---
+++ codeup/1280cu.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a,b;
+    cin>>a>>b;
+
+    int sum=0;
+
+    for(int i=a; i<=b; i++){
+
+        if(i%2==0){
+            cout<<"-"<<i;
+            sum-=i;
+        }
+
+        else {
+            cout<<"+"<<i;
+            sum+=i;
+    }
+    }
+
+    cout<<"="<<sum;
+
+
+}
