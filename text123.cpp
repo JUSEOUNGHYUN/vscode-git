036e538ff3dba8e1018d2410200c6760296bd661
succeed3256@naver.com <succeed3256@naver.com>
2021-08-18 14:39:38 +0900

    자동차 부품 옵션 개수 곱하기.file

--- text123.cpp
+++ text123.cpp
@@ -4,16 +4,20 @@ using namespace std;
 
 int main() {
 
-    int a,b,t;
-    cin>>a>>b;
-    int tmp=0;
+    int test;   // 테스트 케이스 수
+    cin>>test;
 
-    for(int i=a; i<=89; i+=5){
-        tmp++;
-        t=b+tmp;
+    for(int i=0; i<test; i++){
+        int s;  // 자동차 가격    
+        cin>>s;
+
+        int n;  // 자동차 옵션 종류
+        cin>>n;
+        for(int i=0; i<n; i++){
+            int q,p;  // 자동차 옵션1 , 개수  
+            cin>>q>>p;
+            s+=q*p;
+        }
+        cout<<s<<"\n";
     }
-
-    cout<<t;
-
-
 }
