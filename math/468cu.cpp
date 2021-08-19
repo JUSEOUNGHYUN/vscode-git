790090a103089e01915ec6e7aa2c8e3b50dab183
succeed3256@naver.com <succeed3256@naver.com>
2021-08-19 11:35:21 +0900

    codeup 4681 검증수.file

---
+++ math/468cu.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    int sum=0;
+    for(int i=0; i<5; i++){
+        cin>>n;
+        sum+=n*n;
+    }
+    cout<<sum%10;
+}
