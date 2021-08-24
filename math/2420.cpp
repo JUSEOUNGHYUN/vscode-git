85ba77d378e3521ff7c51715bbe0cdd1f0160592
succeed3256@naver.com <succeed3256@naver.com>
2021-08-24 23:43:21 +0900

    절댓값.file

---
+++ math/2420.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib> // abs for int, long int
+       //<cmath>      abs for float, double
+
+using namespace std;
+
+int main() {
+
+    long int a,b;
+    cin>>a>>b;
+
+    cout<<abs(a-b);
+}
