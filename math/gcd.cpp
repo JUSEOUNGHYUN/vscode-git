d8882d41c715e199c5f7ea15990c05e607e29ffd
JUSEOUNGHYUN <succeed3256@naver.com>
2021-08-31 23:57:08 +0900

    최소공배수와 최대공약수.file

---
+++ math/gcd.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b){
+    while (b!=0)
+    {
+        int r=a%b;
+        a=b;
+        b=r;
+    }
+
+    return a;
+}
+
+int lcm(int a, int b){
+    
+    return a*b/gcd(a,b);
+}
+
+int main() {
+
+    int test;
+    cin>>test;
+
+    for(int i=0; i<test; ++i){
+        int a,b;
+        cin>>a>>b;
+
+        cout<<lcm(a,b)<<"\n";
+    }
+
+    return 0;
+}
