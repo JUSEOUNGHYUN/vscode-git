38e751aef1b16e54b4234220565f409cfe6e47d0
JUSEOUNGHYUN <succeed3256@naver.com>
2021-09-06 23:18:17 +0900

    더했을때 10의자리로 넘어가야되는데 그냥 나오는코드.file

---
+++ baekjoonC++/8949.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+ 
+ 
+using namespace std;
+ 
+int swipe(string &a, string &b) {
+    string temp;
+    if (a.size() >= b.size()) {
+        return 1;
+    }
+    temp = b;
+    b = a;
+    a = temp;
+    
+    return 0;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    string input1;
+    string input2;
+    int answer[20];
+ 
+    cin >> input1 >> input2;
+    swipe(input1, input2);
+//    cout << input1 << " " << input2;
+ 
+    int dif = input1.size() - input2.size();
+    for (int i = 0; i < input1.size(); i++) {
+        if (i - dif >=0) {
+            //그냥 내려놓고 통과하면됨 ㅇㅈ?
+            answer[i] = input1[i] + input2[i - dif] - '0' - '0';
+        }
+        else {
+            answer[i] = input1[i]-'0';
+            
+        }
+    }
+    for(int i=0;i<input1.size();i++)
+        cout << answer[i];
+}
