3cee64fe6b0a6e074135ed2233ef136275613ee4
JUSEOUNGHYUN <succeed3256@naver.com>
2021-11-01 16:48:27 +0900

    세 수중에서 가장 작은수 출력.file

---
+++ vector/1150cu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    vector <int> vec(3);
+    
+    for(int i=0; i<3; i++){
+        cin>>vec[i];
+    }
+
+    sort(vec.begin(), vec.end());
+
+    cout<<vec[0];
+}
