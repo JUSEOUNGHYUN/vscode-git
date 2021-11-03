b9b7032e9b873b035936da2e1bacfa99c09b4d7c
JUSEOUNGHYUN <succeed3256@naver.com>
2021-11-03 16:37:17 +0900

    배열 두번 출력하기.file

---
+++ array/1403cu.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int test;
+    cin>>test;
+
+    int a[100];
+
+    for(int i=0; i<test; i++){
+        cin>>a[i];
+    }
+
+    for(int i=0; i<test; i++){
+        cout<<a[i]<<"\n";
+    }
+    
+    for(int i=0; i<test; i++){
+        cout<<a[i]<<"\n";
+    }
+
+    return 0;
+
+}
