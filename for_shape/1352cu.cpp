ac8c8c877d04168031055dbb20f77fbb2bab6e11
JUSEOUNGHYUN <succeed3256@naver.com>
2021-11-12 18:38:03 +0900

    사각형.file

---
+++ for_shape/1352cu.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int test;
+    cin>>test;
+
+    for(int i=1; i<=test; i++){
+        for(int j=1; j<=test; j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+// ***
+// ***
+// ***
