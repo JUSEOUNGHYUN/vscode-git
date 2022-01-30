5238824f5534d184811f3ffe0fbc1b6b7ffa1505
JUSEOUNGHYUN <succeed3256@naver.com>
2022-01-31 06:42:15 +0900

    팰린드롬인지 확인하는 코드.file

---
+++ string/10988.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string str;
+    cin>>str;
+
+    int len = str.length();
+
+    int cnt=0;
+/*
+    for(int i=0; i<len/2; i++){
+        if(str[i]==str[str.length()-1-i]){
+            cnt++;
+    }
+}
+    if(cnt==len/2){
+        cout<<"1";
+    }
+    else{
+        cout<<"0";
+    }
+    return 0;
+    */
+
+    for(int i=0; i<len/2; i++){
+        if(str[i]!=str[str.length()-1-i]){
+            cout<<"0"<<"\n";
+            return 0;
+        }
+    }
+    cout<<"1"<<"\n";
+}
