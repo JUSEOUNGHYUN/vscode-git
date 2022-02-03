a38b49a6753995d3ddd23205118fd122d18772ef
JUSEOUNGHYUN <succeed3256@naver.com>
2022-02-03 16:20:46 +0900

    n의 약수가 아닌 수중에서 min, max값 구하는 코드.file

---
+++ baekjoonC++/8000/8658.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin>>n;
+
+    for(int i=2; i<n-1; i++){
+        if(n%i){
+            cout<<i<<" ";
+            break;
+        }
+    }
+
+    for(int i=n-1; i>=2; i++){
+        if(n%i){
+            cout<<i<<" ";
+            break;
+        }
+    }
+    
+    return 0;
+}
