d6bc9565ef267fd912b2d0b54393d717932f8b94
JUSEOUNGHYUN <succeed3256@naver.com>
2022-02-13 19:09:05 +0900

    알파벳 B.file

---
+++ for_shape/Alphabet/B.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0||i==n/2||i==n-1){
+                if(j==n-1){
+                    cout<<" ";
+                }
+                else{
+                    cout<<"*";
+                }
+            }
+            else{
+                if(j==0|j==n-1){
+                    cout<<"*";
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+// B
+// n = 5
+/*
+**** 
+*   *
+****
+*   *
+****
+*/
